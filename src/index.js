3730757e09a1dc86bc09d8a32c933d53abae7e44
Kevin Marville <kevinmarville@gmail.com>
2024-03-30 11:46:57 +0100

    Update import statement for app name in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
+import { name as pdf } from './app.json';
 import { generatePDF } from './pdfGenerator';
 
 // Register the app component for both native and web
