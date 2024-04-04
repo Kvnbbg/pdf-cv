92d9f108b8df28cb04f52f40364d2930198ca80b
Kevin Marville <kevinmarville@gmail.com>
2024-04-04 17:42:44 +0200

    Add es6-shim and snack dependencies

--- pdf-cv/src/app.js
+++ pdf-cv/src/app.js
@@ -53,7 +53,7 @@ export default function App() {
             <>
               <ActivityIndicator size="large" color="#0000ff" />
               <LottieView
-                source={require('./assets/lottie-animation.json')}
+                source={require('/assets/lottie-animation.json')}
                 autoPlay
                 loop
               />
