b1ae5d679bef9d77a4c7ee942840530adb631f9e
Kevin Marville <kevinmarville@gmail.com>
2024-04-06 20:12:53 +0200

    Update import and registerComponent in index.js, add description in app.json

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {AppRegistry} from 'react-native';
 import App from './pdf-cv/src/app';
-import {name as appName} from './app.json';
+import {name as main} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(main, () => App);
 ReactDOM.render(
   <React.StrictMode>
     <App />
